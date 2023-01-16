d725da9fe1dfc6870b107fd8dbbaecaf201ac772
SamuelParker <samuel.parker@spirit-digital.co.uk>
2023-01-16 11:55:13 +0000

    Mock homepage

--- WeDevlopNow/WeDevelopNowApplication/WeDevelopNowApplication/Form1.cs
+++ WeDevlopNow/WeDevelopNowApplication/WeDevelopNowApplication/Form1.cs
@@ -11,5 +11,30 @@ namespace WeDevelopNowApplication
         {
 
         }
+
+        private void btnLadiesHomepage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBoysHomepage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGirlsHomepage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrandHomepage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrandTypeHomepage_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
