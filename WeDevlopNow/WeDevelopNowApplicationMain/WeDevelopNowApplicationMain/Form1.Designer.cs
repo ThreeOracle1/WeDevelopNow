4588a4d8bb3c40d96ea771e602c7e7b4906edc65
SamuelParker <samuel.parker@spirit-digital.co.uk>
2023-01-16 12:42:51 +0000

    Changing Windows Forms to correct version

---
+++ WeDevlopNow/WeDevelopNowApplicationMain/WeDevelopNowApplicationMain/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WeDevelopNowApplicationMain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlHomepage = new System.Windows.Forms.Panel();
+            this.pnlMens = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlHomepage.SuspendLayout();
+            this.pnlMens.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHomepage
+            // 
+            this.pnlHomepage.Controls.Add(this.label2);
+            this.pnlHomepage.Controls.Add(this.button6);
+            this.pnlHomepage.Controls.Add(this.button5);
+            this.pnlHomepage.Controls.Add(this.button4);
+            this.pnlHomepage.Controls.Add(this.button3);
+            this.pnlHomepage.Controls.Add(this.button2);
+            this.pnlHomepage.Controls.Add(this.button1);
+            this.pnlHomepage.Controls.Add(pictureBox1);
+            this.pnlHomepage.Location = new System.Drawing.Point(21, 12);
+            this.pnlHomepage.Name = "pnlHomepage";
+            this.pnlHomepage.Size = new System.Drawing.Size(425, 700);
+            this.pnlHomepage.TabIndex = 0;
+            // 
+            // pnlMens
+            // 
+            this.pnlMens.Controls.Add(this.pictureBox2);
+            this.pnlMens.Controls.Add(this.label1);
+            this.pnlMens.Location = new System.Drawing.Point(452, 12);
+            this.pnlMens.Name = "pnlMens";
+            this.pnlMens.Size = new System.Drawing.Size(425, 700);
+            this.pnlMens.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            pictureBox1.Location = new System.Drawing.Point(120, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(180, 192);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F);
+            this.label1.Location = new System.Drawing.Point(33, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Men";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(136, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(181, 192);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button1.Location = new System.Drawing.Point(76, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 125);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Men";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button2.Location = new System.Drawing.Point(224, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 125);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Women";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button3.Location = new System.Drawing.Point(76, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 125);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Boys";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button4.Location = new System.Drawing.Point(224, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 125);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Girls";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button5.Location = new System.Drawing.Point(76, 537);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 125);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Brand";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(255)))), ((int)(((byte)(252)))));
+            this.button6.Location = new System.Drawing.Point(224, 537);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 125);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Brand Type";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Homepage";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(883, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 700);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Women";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlHomepage);
+            this.Controls.Add(this.pnlMens);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlHomepage.ResumeLayout(false);
+            this.pnlHomepage.PerformLayout();
+            this.pnlMens.ResumeLayout(false);
+            this.pnlMens.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHomepage;
+        private System.Windows.Forms.Panel pnlMens;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
