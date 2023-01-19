dfe8e26791cb6073c6dd524e63e641068301d72b
SamuelParker <samuel.parker@spirit-digital.co.uk>
2023-01-19 17:08:40 +0000

    Created unit test project and first Unit test

---
+++ WeDevlopNow/WeDevelopNowApplicationMain/WeDevelopNowMain.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeDevelopNowMain.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeDevelopNowMain.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3a8ab8d-bf98-487f-999b-4dc87b039a37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeDevlopNow/WeDevelopNowApplicationMain/WeDevelopNowMain.Tests/WeDevelopNowUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using WeDevelopNowApplicationMain;
+
+namespace WeDevelopNowMain.Tests
+{
+    [TestClass]
+    public class WeDevelopNowUnitTests
+    {
+        [TestMethod]
+        public void MenGerneratesValidSqlStatementTemplate()
+        {
+            var MensSearchScreen = new UserControlMenSearchScreen();
+
+            var MensSearchResultScreen = new UserControlMenSearchResultScreen();
+
+            string MensSqlQuery = MensSearchScreen.FindTableSearchQueryMen();
+
+            MensSearchResultScreen.BindDataGridMenFindResult(MensSqlQuery);
+
+            string exampleSQLQuery = "SELECT [Product Discription], Brands , Quantity, Location FROM OurProducts WHERE [Product Type] ='' AND [Mens Sizes] like '%%' AND Colour = '' AND Price BETWEEN '0' AND '0' AND Brands = ''";
+
+            MensSqlQuery.Should().Be(exampleSQLQuery);
+
+        }
+    }
+}
